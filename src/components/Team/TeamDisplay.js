78bcaa673b19a6f7084c666084cc0b174f82e4e5
Tyler Hawkins <thawkinsusa@gmail.com>
2019-08-06 20:56:05 -0600

    battle arena

--- src/components/Team/TeamDisplay.js
+++ src/components/Team/TeamDisplay.js
@@ -1,21 +1,55 @@
 import React, { Component } from 'react'
 import './TeamDisplay.css'
-class TeamDisplay extends Component {
+class BattleDisplay extends Component {
+    constructor(){
+        super()
+        this.state={
+            displayAttackOnePunch:false,
+            displayAttackGenos:false
+        }
+    }
+
+    toggleOne = () => {
+        this.setState({ displayAttackOnePunch: !this.state.displayAttackOnePunch })
+
+    }
     render() {
         return (
-            <div className='main-container'>main continer
-                <div className='main-left-right-split'>
-                    <div className='main-left-container'>dash continer
-                    <div className='left-container-info'>Info/dash</div>
-                        <div className='left-container-info-second'>Info/dash</div>
+            <div>
+                <img src='http://www.sclance.com/backgrounds/cartoon-backgrounds/cartoon-backgrounds_488267.png' className='main-container' />main continer
+                {
+                    this.state.displayAttackOnePunch
+                        ? (
+                            <div>
+                                <img src='https://media1.giphy.com/media/oZTBL4vOswrT2/giphy.gif?cid=790b76115d4a38cb4b5357366f55d650&rid=giphy.gif' className='main-container'/>
+                            </div>
+                        )
+                        : (
+                            null
+                        )
+                }
+                <div className='char-list'>
+                    <div className='img-holder-one'>
+                        <div className='img-one-background'>
+                <img src='https://i.ytimg.com/vi/JzqYAj0FDeo/maxresdefault.jpg'className='image-container-1' />
+                <progress id="health" value='100' max="100"></progress>
+                            <button onClick={this.toggleOne}>Attack</button>
+                        </div>
                     </div>
-                    <div className='main-right-container'>right side container
-                    <div className='right-container-info'>Team Info</div>
-                        <div className='right-container-info-second'>Team Info</div>
                     </div>
+                        <div className='char-info'>
                 </div>
+                <div className='char-list-2'>
+                    <div className='img-holder-2'>
+                        <div className='img-2-background'>
+                <img src='https://i.ytimg.com/vi/JzqYAj0FDeo/maxresdefault.jpg'className='image-container-2' />
+                <progress id="health" value='100' max="100"></progress>
+                            <button onClick={this.toggleOne}>Attack</button>
+                        </div>
+                    </div>
+                    </div>
             </div>
         )
     }
 }
-export default TeamDisplay
+export default BattleDisplay
